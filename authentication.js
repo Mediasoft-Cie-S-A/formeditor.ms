7abc54eb6336b2c3f8a9e8b4507b42900a1c6ab7
mserpadmin <151834587+mserpadmin@users.noreply.github.com>
2023-12-30 07:59:09 +0100

    AzureAd

--- authentication.js
+++ authentication.js
@@ -14,9 +14,10 @@
  * limitations under the License.
  */
 
-const LocalStrategy = require('passport-local').Strategy;
+const OIDCStrategy = require('passport-azure-ad').OIDCStrategy;
 
 module.exports = function(app,session, passport) {
+
 
  // allow passport to use "express-session".
  app.use(session({
@@ -25,111 +26,110 @@ module.exports = function(app,session, passport) {
     saveUninitialized: true ,
   }))
  
-    // This is the basic express session({..}) initialization.
+// This is the basic express session({..}) initialization.
  app.use(passport.initialize()) 
  // init passport on every route call.
  app.use(passport.session())    
 
-
-    authUser = (user, password, done) => {
-        console.log(`Value of "User" in authUser function ----> ${user}`)         //passport will populate, user = req.body.username
-        console.log(`Value of "Password" in authUser function ----> ${password}`) //passport will popuplate, password = req.body.password
-    
-    // Use the "user" and "password" to search the DB and match user/password to authenticate the user
-    // 1. If the user not found, done (null, false)
-    // 2. If the password does not match, done (null, false)
-    // 3. If user found and password match, done (null, user)
-        
-        let authenticated_user = { id: 123, name: "admin"} 
-    //Let's assume that DB search that user found and password matched for Kyle
-        
-        return done (null, authenticated_user ) 
+ // Configure the Azure AD strategy for Passport
+passport.use(new OIDCStrategy({
+    identityMetadata: `https://login.microsoftonline.com/89218426-003e-4883-a0d3-445bcca6f8c6/v2.0/.well-known/openid-configuration`,
+    clientID: '8ceb09f3-fdc2-4a87-b5e6-067a7db86482',
+    responseType: 'code',
+    responseMode: 'query',
+    redirectUrl: 'http://localhost:3000/auth/openid/return',
+    allowHttpForRedirectUrl: true,
+    clientSecret: '1-K8Q~MTHGtDOLonCupMCnAFkU~TJiewxHaCybaw',
+    validateIssuer: false,
+    passReqToCallback: true,
+    scope: ['profile', 'offline_access', 'https://graph.microsoft.com/mail.read']
+  },
+  (req, iss, sub, profile, accessToken, refreshToken, params, done) => {
+    if (!profile.oid) {
+      return done(new Error("No oid found"), null);
     }
-     
-    passport.use(new LocalStrategy (authUser))
+    // You can save the profile information to your user database here
+    return done(null, profile);
+  }
+));
 
-    passport.serializeUser( (user, done) => { 
-        console.log(`--------> Serialize User`)
-        console.log(user)     
-    
-        done(null, user.id)
-      
-    // Passport will pass the authenticated_user to serializeUser as "user" 
-    // This is the USER object from the done() in auth function
-    // Now attach using done (null, user.id) tie this user to the req.session.passport.user = {id: user.id}, 
-    // so that it is tied to the session object
-    
-    } )
-    
-    
-    passport.deserializeUser((id, done) => {
-            console.log("---------> Deserialize Id")
-            console.log(id)
-    
-            done (null, {name: "masspe", id: 123} )      
-      
-    // This is the id that is saved in req.session.passport.{ user: "id"} during the serialization
-    // use the id to find the user in the DB and get the user object with user details
-    // pass the USER object in the done() of the de-serializer
-    // this USER object is attached to the "req.user", and can be used anywhere in the App.
-    
-    }) 
-    
-    
-    //Middleware to see how the params are populated by Passport
-    let count = 1
-    
-    printData = (req, res, next) => {
-        /*
-        console.log("\n==============================")
-        console.log(`------------>  ${count++}`)
-    
-        console.log(`req.body.username -------> ${req.body.username}`) 
-        console.log(`req.body.password -------> ${req.body.password}`)
-    
-        console.log(`\n req.session.passport -------> `)
-        console.log(req.session.passport)
-      
-        console.log(`\n req.user -------> `) 
-        console.log(req.user) 
-      
-        console.log("\n Session and Cookie")
-        console.log(`req.session.id -------> ${req.session.id}`) 
-        console.log(`req.session.cookie -------> `) 
-        console.log(req.session.cookie) 
-      
-        console.log("===========================================\n")
-        */
-        next()
-    }
-    
-    app.use(printData) //user printData function as middleware to print populated variables
-    
-    
-    app.get("/login", (req, res) => {
-        res.render("login.ejs")
-    
-    })
-    
-    app.delete("/logout", (req,res) => {
-        req.logOut()
-        res.redirect("/login")
-        console.log(`-------> User Logged out`)
-     })
-    
-    app.post ("/login", passport.authenticate('local', {
-        successRedirect: "/dashboard",
-        failureRedirect: "/login",
-    }))
-    
-    
-    app.post ("/login", passport.authenticate('local', {
-        successRedirect: "/dahsboard.ejs",
-        failureRedirect: "/login",
-     }))
-    
+passport.serializeUser((user, done) => {
+    done(null, user.oid);
+});
+
+passport.deserializeUser((oid, done) => {
+    // Find the user by OID and return the user object
+    // Implement your logic to retrieve user from your database or user store
+    done(null, { oid: oid, displayName: 'User Display Name' });
+});
+
+// Authentication request
+app.get('/auth/openid',
+  passport.authenticate('azuread-openidconnect', { failureRedirect: '/login', 
+  failureMessage: true // This enables failure messages to be provided when authentication fails.
+ }),  
+  (req, res) => {
+    res.redirect('/');
+  }
+);
 
 
+// Logout
+app.get('/logout', (req, res) => {
+  req.session.destroy((err) => {
+    req.logout();
+    res.redirect('/');
+  });
+});
+
+// ... [previous code]
+
+// Authentication callback
+app.get('/auth/openid/return', 
+    passport.authenticate('azuread-openidconnect', { failureRedirect: '/login'    , 
+  failureMessage: true // This enables failure messages to be provided when authentication fails.
+ }),
+   (req, res) => {
+    // Authentication was successful
+    // get username from req.user.oid
+    const username = req.user.oid;
+    res.send(`
+      <!DOCTYPE html>
+      <html>
+      <head>
+        <title>Authentication Successful</title>
+      </head>
+      <body>
+        <h1>Authentication Successful</h1>
+        <p>Welcome,${username}!</p>
+        <a href="/Dashboard">Go to Home</a>
+        <a href="/logout">Logout</a>
+      </body>
+      </html>
+    `);
+  }
+);
+
+// Login route
+app.get('/login', (req, res) => {
+    const errorMessage = req.session.messages || [];
+    req.session.messages = []; // Clear the message so it doesn't reappear on subsequent visits.
+  
+    res.send(`
+      <!DOCTYPE html>
+      <html>
+      <head>
+        <title>Login</title>
+      </head>
+      <body>
+        <h1>Login</h1>
+        ${errorMessage.length > 0 ? `<p style="color: red;">Error: ${errorMessage.join('<br>')}</p>` : ''}
+        <p>Please <a href="/auth/openid">login with Azure AD</a>.</p>
+      </body>
+      </html>
+    `);
+  });
+  
 
 
 };
