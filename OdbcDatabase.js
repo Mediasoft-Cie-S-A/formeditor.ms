8772d62cd6862e339e037787a80c7214e78c887f
mserpadmin <151834587+mserpadmin@users.noreply.github.com>
2024-02-07 08:51:08 +0100

    fix record read

--- OdbcDatabase.js
+++ OdbcDatabase.js
@@ -56,7 +56,7 @@ class OdbcDatabase {
             // Construct the SQL statement
             // if filter is not empty, add it to the query
 
-            var sql = `SELECT DISTINCT ${columnName} FROM PUB.${tableName} `;	
+            var sql = `SELECT DISTINCT "${columnName}" FROM PUB.${tableName} `;	
             if (filter && filter.length > 0) {
                 sql+=` WHERE ${filter} `;
             }
@@ -98,7 +98,7 @@ class OdbcDatabase {
             const result = await this.connection.query(queryString);
             return result;
         } catch (err) {
-            console.log('Error querying data:', err);
+            console.log('Error querying data:', queryString, err);
            // throw err;
         }
     }
@@ -208,7 +208,8 @@ class OdbcDatabase {
              }  
 
             if (fields && fields.length > 0) {
-                const fieldList = fields.join(', ');
+                // Construct the SQL query based on the fields provided by fieldList and adding "" to the field name
+                const fieldList = fields.map(field => `"${field}"`).join(', ').replace("\"rowid\"","rowid");
                 const offset = (page - 1) * pageSize;
                 
                 var paginatedQuery = `select  ${fieldList} FROM PUB."${tableName}"`;
@@ -234,7 +235,7 @@ class OdbcDatabase {
     // Construct the SQL query based on the fields provided
     let query;
     if (fields && fields.length > 0) {
-        const fieldList = fields.join(', ');
+        const fieldList = fields.map(field => `"${field}"`).join(', ').replace("\"rowid\"","rowid");
         console.log(fieldList);
         const query = `SELECT TOP 1 ${fieldList} FROM PUB.${tableName} `;
         return this.queryData(query);
@@ -247,7 +248,7 @@ async moveToLast(tableName , fields) {
     // OpenEdge doesn't have a direct way to select the last record, so you might need to use an ORDER BY clause
     // with DESC and then select the TOP 1 record. This assumes you have a column to order by.
     if (fields && fields.length > 0) {
-        const fieldList = fields.join(', ');
+        const fieldList = fields.map(field => `"${field}"`).join(', ').replace("\"rowid\"","rowid");
         const query = `SELECT ${fieldList} FROM PUB.${tableName} ORDER BY 1 desc `;    
         return this.queryData(query);
     }
@@ -257,7 +258,7 @@ async moveToLast(tableName , fields) {
 // Move to the next record
 async moveToNext(tableName, fields, currentRowId) {
     if (fields && fields.length > 0) {
-        const fieldList = fields.join(', ');
+        const fieldList = fields.map(field => `"${field}"`).join(', ').replace("\"rowid\"","rowid");
         // Assuming 'currentRowId' is the ROWID of the current record
         const query = `SELECT ${fieldList} FROM PUB.${tableName}  OFFSET ${currentRowId} ROWS FETCH NEXT 1 ROWS ONLY`;
         console.log(query);
@@ -272,7 +273,7 @@ async moveToPrevious(tableName, fields, currentRowId) {
     // This is a bit tricky as OpenEdge doesn't support fetching the previous record directly
     // You might need to fetch all records with ROWID less than the current one and then take the last one
     if (fields && fields.length > 0) {
-        const fieldList = fields.join(', ');
+        const fieldList = fields.map(field => `"${field}"`).join(', ').replace("\"rowid\"","rowid");
             const query = `SELECT ${fieldList} FROM PUB.${tableName} OFFSET ${currentRowId} ROWS FETCH NEXT 1 ROWS ONLY`;
             console.log(query);
             return this.queryData(query);
@@ -284,7 +285,7 @@ async moveToPrevious(tableName, fields, currentRowId) {
 async getRecordByRowID(tableName, fields, rowID) {
     // Assuming 'rowID' is the ROWID of the record to move to
     if (fields && fields.length > 0) {
-        const fieldList = fields.join(', ');
+        const fieldList = fields.map(field => `"${field}"`).join(', ').replace("\"rowid\"","rowid");
         const query = `SELECT ${fieldList} FROM PUB.${tableName} WHERE ROWID = '${rowID}'`;
         console.log(query);
         return this.queryData(query);
@@ -296,7 +297,7 @@ async getRecordByRowID(tableName, fields, rowID) {
 // Move to the next record
 async getROWID(tableName, currentRowId) {
     // Assuming 'currentRowId' is the ROWID of the current record
-    const query = `SELECT ROWID FROM PUB.${tableName}  OFFSET ${currentRowId} ROWS FETCH NEXT 1 ROWS ONLY`;
+    const query = `SELECT ROWID FROM PUB."${tableName}"  OFFSET ${currentRowId} ROWS FETCH NEXT 1 ROWS ONLY`;
     console.log(query);
     return this.queryData(query);
 }
@@ -322,6 +323,7 @@ async updateRecord(tableName, data, rowID) {
 async insertRecord(tableName, data) {
     try {
         // Construct the full SQL statement
+        
         const sql = `INSERT INTO PUB.${tableName} (${data.fields}) VALUES (${data.values})`;
         console.log(sql);
         // Execute the query
