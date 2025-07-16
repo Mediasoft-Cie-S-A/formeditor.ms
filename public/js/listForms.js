f4ad24cfd3ac042bb184803c98f9e242d36ff947
Valentin Schneeberger <valentin.schneeberger@mediasoft-sa.ch>
2025-07-16 14:40:21 +0200

    On load render, do also load Editor so navigation si easier

--- public/js/listForms.js
+++ public/js/listForms.js
@@ -15,7 +15,7 @@
  */
 
 
-function loadForms(){
+function loadForms() {
     console.log('Loading forms...');
     fetch('/list-forms')
         .then(response => {
@@ -26,22 +26,22 @@ function loadForms(){
         })
         .then(forms => {
             const list = document.getElementById('componentsListBody');
-            
+
             list.innerHTML = ''; // Clear the list
-           
+
             forms.forEach(form => {
-                 const container = document.createElement('tr'); // Create a new container for the forms
-             
+                const container = document.createElement('tr'); // Create a new container for the forms
+
                 container.className = 'portal-container';
                 container.setAttribute('data-form-id', form.objectId); // Set the form ID as a data attribute
                 container.setAttribute('title', `double click to view form`);
                 const ItemID = document.createElement('td'); // Create a new list item for each form
                 ItemID.innerHTML = `<b>${form.objectId}</b>`; // Adjust according to your form object structure
                 container.appendChild(ItemID); // Append the list item to the container
-                const ItemName = document.createElement('td');                
+                const ItemName = document.createElement('td');
                 ItemName.innerHTML = `<b>${form.objectName}</b>`; // Adjust according to your form object structure
-                      
-                
+
+
                 container.appendChild(ItemName); // Append the list item to the container
                 const ItemSlug = document.createElement('td');
                 ItemSlug.innerHTML = `<i>${form.objectSlug}</i>`; // Adjust according to your form object structure
@@ -55,71 +55,73 @@ function loadForms(){
                 const ItemDate = document.createElement('td');
                 ItemDate.innerHTML = `<i>${form.modificationDate}</i>`; // Adjust according to your form object structure
                 container.appendChild(ItemDate); // Append the list item to the container
-                
+
                 // Create delete button
                 const deleteButton = document.createElement('button');
-                deleteButton.innerHTML='<i class="fa fa-trash" style="margin-left:-5px"></i>'
-                deleteButton.className = 'portal-delete-button';        
-                deleteButton.onclick = function(event) {
+                deleteButton.innerHTML = '<i class="fa fa-trash" style="margin-left:-5px"></i>'
+                deleteButton.className = 'portal-delete-button';
+                deleteButton.onclick = function (event) {
                     event.preventDefault();
                     deleteForm(form.objectId, listItem);
                 }; // delete button functionality
 
                 // create edit button
                 const editButton = document.createElement('button');
-                editButton.innerHTML='<i class="fa fa-edit" style="margin-left:-5px"></i>'
+                editButton.innerHTML = '<i class="fa fa-edit" style="margin-left:-5px"></i>'
                 editButton.className = 'portal-edit-button';
-                editButton.onclick = function(event) {
+                editButton.onclick = function (event) {
                     event.preventDefault();
-                    loadFormData(form.objectId,document.getElementById('formContainer'));
+                    loadFormData(form.objectId, document.getElementById('formContainer'));
                     const editTab = document.querySelector('.nav-tabs a[href="#editForm"]');
                     if (editTab) {
                         editTab.click(); // Simulate click
                     }  // Simulate click on the edit tab
-                   
+
                 }; // edit button functionality
                 // create show button
                 const showButton = document.createElement('button');
-                showButton.innerHTML='<i class="fa fa-eye" style="margin-left:-5px"></i>'
+                showButton.innerHTML = '<i class="fa fa-eye" style="margin-left:-5px"></i>'
                 showButton.className = 'portal-show-button';
-                showButton.onclick = function(event) {
+                showButton.onclick = function (event) {
                     event.preventDefault();
-                    loadFormData(form.objectId,document.getElementById('renderContainer'));
+                    loadFormData(form.objectId, document.getElementById('renderContainer'));
+                    loadFormData(form.objectId, document.getElementById('formContainer'));
                     const showTab = document.querySelector('.nav-tabs a[href="#renderForm"]');
                     if (showTab) {
                         showTab.click(); // Simulate click
                     }// if (showTab) {
-                   
+
                 }; // show button functionality
                 const itemActions = document.createElement('td');
                 container.appendChild(itemActions); // Append the actions cell to the container
                 //append the delete button to the list item
-                
-                itemActions.appendChild(showButton);                
+
+                itemActions.appendChild(showButton);
                 itemActions.appendChild(editButton);
                 itemActions.appendChild(deleteButton);
-                container.addEventListener('dblclick', function(event) {
+                container.addEventListener('dblclick', function (event) {
                     event.preventDefault();
-                    loadFormData(form.objectId,document.getElementById('renderContainer'));
+                    loadFormData(form.objectId, document.getElementById('renderContainer'));
+                    loadFormData(form.objectId, document.getElementById('formContainer'));
                     const showTab = document.querySelector('.nav-tabs a[href="#renderForm"]');
                     if (showTab) {
                         showTab.click(); // Simulate click
                     }
                 }); // Add a double-click event listener to the list item
-                container.addEventListener('click', function(e) {
+                container.addEventListener('click', function (e) {
                     e.preventDefault();
                     // check if the event is a click on the delete,show or edit button
-                    if (e.target.className === 'portal-list-item'){
-                        
-                    showHint(`ID:${form.objectId}<br>User:${form.userCreated}<br>Last mod:${form.modificationDate}`, 5000, event);
+                    if (e.target.className === 'portal-list-item') {
+
+                        showHint(`ID:${form.objectId}<br>User:${form.userCreated}<br>Last mod:${form.modificationDate}`, 5000, event);
                     }
                 }
                 ); // ListItem.addEventListener 
-        
-             
-                 list.appendChild(container); // Append the container to the list
+
+
+                list.appendChild(container); // Append the container to the list
                 // Add the container to the list
-        }); // forEach
+            }); // forEach
             // Check if there are no forms
             if (forms.length === 0) {
                 const noFormsMessage = document.createElement('div');
@@ -131,28 +133,28 @@ function loadForms(){
             // Manually trigger the search functions
             const searchIdInput = document.getElementById('searchFormInput');
             if (searchIdInput && searchIdInput.value) {
-                searchFormbyID({ target: searchIdInput, preventDefault: () => {} });
+                searchFormbyID({ target: searchIdInput, preventDefault: () => { } });
             }
 
             // Manually trigger the search functions
             const searchNameInput = document.getElementById('searchNameInput');
             if (searchNameInput && searchNameInput.value) {
-                searchFormbyName({ target: searchNameInput, preventDefault: () => {} });
+                searchFormbyName({ target: searchNameInput, preventDefault: () => { } });
             }
-            
+
             // Manually trigger the search functions
             const searchSlugInput = document.getElementById('searchSlugInput');
             if (searchSlugInput && searchSlugInput.value) {
-                searchFormbySlug({ target: searchSlugInput, preventDefault: () => {} });
+                searchFormbySlug({ target: searchSlugInput, preventDefault: () => { } });
             }
-            
+
 
         })// Handle the response
         .catch(error => {
             console.error('There was a problem with the fetch operation:', error);
         });
 
-    
+
 }
 
 function deleteForm(objectId, listItem) {
@@ -213,7 +215,7 @@ function searchFormbyName(event) {
         } else {
             row.style.display = 'none'; // Hide the row if it doesn't match
         }
-    }); 
+    });
     if (!found) {
         showToast('No forms found with Name: ' + searchTerm, 5000); // Show a toast message if no forms are found
     }
@@ -244,9 +246,9 @@ function searchFormbySlug(event) {
     console.log('Search completed.');
 }
 
-function loadFormData(objectId,renderContainer)
- {
-     fetch(`/get-form/${objectId}`)
+function loadFormData(objectId, renderContainer) {
+    console.log("Load form data");
+    fetch(`/get-form/${objectId}`)
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
@@ -254,16 +256,16 @@ function loadFormData(objectId,renderContainer)
             return response.json();
         })
         .then(form => {
-             // Clear the render container
+            // Clear the render container
 
-             renderContainer.innerHTML = '';
+            renderContainer.innerHTML = '';
 
 
             // Convert JSON back to DOM and append
-            var domContent = jsonToDom(form.formData,renderContainer);
+            var domContent = jsonToDom(form.formData, renderContainer);
             renderElements(renderContainer);
             rebuildComponents(renderContainer);
-           
+
             // set the header
             var objectId = document.getElementById('objectId');
             var objectName = document.getElementById('objectName');
@@ -274,19 +276,19 @@ function loadFormData(objectId,renderContainer)
             // select in the objecttype the value form
             objectType.value = 'form';
             // set the values in the header
-            
+
             // Handle the form data here
             console.log('Form Data:', form);
             // For example, display the form data in an alert or populate a form for editing
-            objectId.value=form.objectId;
+            objectId.value = form.objectId;
             console.log(objectId);
-            objectName.value=form.objectName;
-            objectSlug.value=form.objectSlug;
-            userCreated.value=form.userCreated;
-           
+            objectName.value = form.objectName;
+            objectSlug.value = form.objectSlug;
+            userCreated.value = form.userCreated;
+
         })
         .catch(error => {
-            showToast('Error!'+error, 5000); // Show toast for 5 seconds
+            showToast('Error!' + error, 5000); // Show toast for 5 seconds
             console.error('There was a problem with the fetch operation:', error);
         });
 }
@@ -341,59 +343,57 @@ function loadFormDataInModal(objectId, container) {
 
 function showToast(message, duration = 3000) {
     let toastContainer = document.getElementById("toast-container");
-    if (toastContainer === null || toastContainer === undefined)
-    {
-       toastContainer = document.createElement("div") ;
-       toastContainer.id="toast-container";
+    if (toastContainer === null || toastContainer === undefined) {
+        toastContainer = document.createElement("div");
+        toastContainer.id = "toast-container";
 
     }
     const toast = document.createElement("div");
     toast.classList.add("toast-message");
     toast.textContent = message;
-  
+
     // Add the toast to the container
     toastContainer.appendChild(toast);
-  
+
     // Make the toast visible
     setTimeout(() => {
-      toast.style.opacity = 1;
+        toast.style.opacity = 1;
     }, 100);
-  
+
     // Hide and remove the toast after 'duration'
     setTimeout(() => {
-      toast.style.opacity = 0;
-      toast.addEventListener("transitionend", () => toast.remove());
+        toast.style.opacity = 0;
+        toast.addEventListener("transitionend", () => toast.remove());
     }, duration);
-  }
-  
-  // show hint like a toastmessage wiht absolute position, based on the mouse position
-  function showHint(message, duration = 1000, event) {
+}
+
+// show hint like a toastmessage wiht absolute position, based on the mouse position
+function showHint(message, duration = 1000, event) {
     let hintContainer = document.getElementById("hint-container");
-    if (hintContainer === null || hintContainer === undefined)
-        {
-            hintContainer = document.createElement("div") ;
-            hintContainer.id="toast-container";
-    
-        }
+    if (hintContainer === null || hintContainer === undefined) {
+        hintContainer = document.createElement("div");
+        hintContainer.id = "toast-container";
+
+    }
     hintContainer.style.top = 10 + event.clientY + "px";
     hintContainer.style.left = 10 + event.clientX + "px";
     const hint = document.createElement("div");
     hint.classList.add("hint-message");
     hint.innerHTML = message;
-  
+
     hintContainer.innerHTML = "";
-  
+
     // Add the toast to the container
     hintContainer.appendChild(hint);
-  
+
     // Make the toast visible
     setTimeout(() => {
-      hint.style.opacity = 1;
+        hint.style.opacity = 1;
     }, 100);
-  
+
     // Hide and remove the toast after 'duration'
     setTimeout(() => {
-      hint.style.opacity = 0;
-      hint.addEventListener("transitionend", () => hint.remove());
+        hint.style.opacity = 0;
+        hint.addEventListener("transitionend", () => hint.remove());
     }, duration);
-  }
+}
